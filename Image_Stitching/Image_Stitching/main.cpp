db135bb180b65981ba706b2bd35d5d918ba14379
bangjinhyuk <jinhyuk0103@gmail.com>
2021-12-21 23:11:58 +0900

    Create Image Stitching project

---
+++ Image_Stitching/Image_Stitching/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Image_Stitching
+//
+//  Created by 방진혁 on 2021/12/21.
+//
+
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp> //기본적인 기능
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp> // 이미지를 키우거나 줄이거나 하는
+#include <opencv2/highgui.hpp> //highlevel grapic
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
